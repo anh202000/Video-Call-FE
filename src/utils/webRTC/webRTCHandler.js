e356546bd2ae8f948fe642dc3a1fd982a722c09d
Le Phan tuan Anh <anhroyal110@gmail.com>
2022-01-22 09:38:43 +0700

    AnhLPT 22/1 manage our by create call Sate redux

--- src/utils/webRTC/webRTCHandler.js
+++ src/utils/webRTC/webRTCHandler.js
@@ -1,4 +1,4 @@
-import { setLocalStream } from "../../store/actions/callActions"
+import { callStates, setCallState, setLocalStream } from "../../store/actions/callActions"
 import store from '../../store/store'
 
 const defaultConstrains = {
@@ -10,6 +10,7 @@ export const getLocalStream = () => {
     navigator.mediaDevices.getUserMedia(defaultConstrains)
     .then(stream => {
         store.dispatch(setLocalStream(stream));
+        store.dispatch(setCallState(callStates.CALL_AVAILABLE));
     })
     .catch((err => {
         console.log(' error occured when trying to get an access to get local stream');
